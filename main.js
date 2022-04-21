605d87c469734a0db7709f31d31c07eb9e809f71
MacMini18 <mcm-mac-18@MacMini18s-Mac-mini.local>
2022-04-21 11:58:18 -0700

    done

---
+++ main.js
@@ -0,0 +1,50 @@
+function equal(a,b){
+
+    
+
+
+    return (a+b==1000)||(a==1000)||(b==1000);
+}
+
+console.log(equal(1000,35))
+
+/*a='index.html'.split('.')
+console.log(a)*/
+
+function extention(s){
+ let a=s.split('.')
+ return a[1]
+
+}
+
+
+var a=['main.js','index.html','styles.css']
+
+ 
+ for(var i=0;i<a.length;i++){
+     console.log(extention(a[i]))
+ }
+
+
+ function calFarenheit(Celsius){
+     return  9/5*Celsius+32
+ }
+
+ console.log(calFarenheit(19))
+
+ function DogAge(age){
+     return "Your doggie is "+age*7+" years old in dog years";
+ }
+
+ var n=[8,5,13]
+
+ 
+  
+  for(var i=0;i<n.length;i++){
+      console.log(DogAge(n[i]))
+
+      }
+  
+  function Lifetime(age, ammount){
+      return 
+  }
